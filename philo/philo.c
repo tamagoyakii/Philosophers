a4a32ebb653a98c8684d4e524bdad8479b4c7f03
tamagoyakii <rlawlgus4063@naver.com>
2022-08-28 17:21:17 +0900

    Philosophers

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: jihyukim <jihyukim@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 15:20:18 by jihyukim          #+#    #+#             */
-/*   Updated: 2022/08/28 17:08:12 by jihyukim         ###   ########.fr       */
+/*   Updated: 2022/08/28 17:20:13 by jihyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 void	morintoring(t_info *info, t_philo **philo)
 {
 	int	n_full_philo;
-	int	reason;
 	int	i;
 
-	reason = DIE;
 	i = -1;
 	while (1)
 	{
@@ -39,14 +37,9 @@ int	is_dead(t_info *info, t_philo *philo)
 	long long	now;
 	int			dead;
 
-	pthread_mutex_lock(&(info->check_death));
-	dead = info->is_dead;
-	pthread_mutex_unlock(&(info->check_death));
-	if (dead)
-		return (dead);
+	dead = 0;
 	now = get_time();
 	pthread_mutex_lock(&(info->check_last_eat));
-	pthread_mutex_unlock(&(info->check_last_eat));
 	if (info->t_die < now - philo->t_last_eat)
 	{
 		prints(info, philo->id, DIE);
